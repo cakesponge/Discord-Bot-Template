347726c0fbb29dbef6faca8de774b258ed83f59b
cakesponge <67387897+cakesponge@users.noreply.github.com>
2021-03-27 19:16:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const token = "your token here"; 
+const token = "your token here"; //token
 
 const fs = require('fs'); //installed along with Node
 
@@ -11,13 +11,13 @@ fs.readdir("./commands/", (err, files) => {
 
     if(err) console.log(err)
 
-    let jsfile = files.filter(f => f.split(".").pop() === "js") 
+    let jsfile = files.filter(f => f.split(".").pop() === "js") //finds js files
     if(jsfile.length <= 0) {
          return console.log("[LOGS] Couldn't Find Commands!");
     }
 
     jsfile.forEach((f, i) => {
-        let pull = require(./commands/${f});
+        let pull = require(`./commands/${f}`);
         bot.commands.set(pull.config.name, pull);
         pull.config.aliases.forEach(alias => {
             bot.aliases.set(alias, pull.config.name)
@@ -25,13 +25,13 @@ fs.readdir("./commands/", (err, files) => {
     });
 });
 
-bot.once('ready',() => {
+bot.on('ready',() => {
     console.log('The bot is online.')
 });
 
 bot.on("message", async message => {
 
-    if(message.author.bot  message.channel.type === "dm") return;
+    if(message.author.bot || message.channel.type === "dm") return
 
     let prefix = "your prefix here";
     let messageArray = message.content.split(" ");
@@ -39,14 +39,13 @@ bot.on("message", async message => {
     let args = messageArray.slice(1);
 
     if(!message.content.startsWith(prefix)) return;
-    let commandfile = bot.commands.get(cmd.slice(prefix.length))  bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
+    let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
     if(commandfile) commandfile.run(bot,message,args)
 
 })
 
 bot.login(token);
 
-
-//Things needed for you to fill out:
-//the prefix on line 36
-//your token on line 3
+//things required to fill out:
+//line 36 change prefix
+//line 3 change to your token
